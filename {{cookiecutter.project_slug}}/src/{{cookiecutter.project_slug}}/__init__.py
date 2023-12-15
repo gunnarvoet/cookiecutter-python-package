237e1943d96cb7631e8febcf0f4e50366ac5a9c5
Gunnar Voet <gvoet@ucsd.edu>
2023-12-15 14:33:46 -0800

    move to src structure

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -0,0 +1,33 @@
+"""
+# Overview
+
+Say something here about the project.
+
+{% if cookiecutter.custom_pdoc_theme == 'y' -%}
+# Markdown Syntax
+## Figures
+Narrow image:
+```markdown
+![nimage](path/to/figure)
+```
+Medium sized image:
+```markdown
+![image](path/to/figure)
+```
+Wide image:
+```markdown
+![wimage](path/to/figure)
+```
+{% endif %}
+
+# License
+
+.. include:: ../LICENSE
+"""
+
+__author__ = """{{ cookiecutter.full_name }}"""
+__email__ = '{{ cookiecutter.email }}'
+__version__ = '{{ cookiecutter.version }}'
+
+__all__ = ["io"]
+from . import io
